231258869552e1731dff84e7038b0c6e2e874b50
AlejandroDave <alejandro.davidrd@gmail.com>
2025-07-02 16:54:17 -0600

    Documentacion del codigo

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from backend.app import crear_app
 
-app = crear_app()
+app = crear_app() # se invoca la funcion crear_app desde la carpeta app
 
+
+#Al ser ejecutado se crea la condicion de tener un debug correcto
 if __name__ == '__main__':
     app.run(debug=True)
