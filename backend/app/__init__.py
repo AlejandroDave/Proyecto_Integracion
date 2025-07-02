f403263d95cb375c43783a34127ee476c905c897
AlejandroDave <alejandro.davidrd@gmail.com>
2025-07-01 18:37:37 -0600

    aa

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask  # Importación de la biblioteca
+import backend.rutas.rutas as rutes
+'''
+Base del Back-End:
+Se crea una aplicación Flask cada que se ejecuta la función "crear_app" la cual registrará las rutas indicadas.
+
+'''
 
 
 def crear_app():
     app = Flask(__name__)
-
-
-
-    import backend.rutas.rutas as rutes
-    
+¿
     app.register_blueprint(rutes.front_bp)
     app.register_blueprint(rutes.diagnosticos_bp)
     app.register_blueprint(rutes.usuarios_bp)
@@ -16,6 +18,12 @@ def crear_app():
     app.register_blueprint(rutes.diagnosticoInsert_bp)
 
     return app
+
+
+"""
+Se creara una condicion que permita crear la aplicacion corroborando que es la aplicación principal.
+
+"""
 
 if __name__ == '__main__':
     app = crear_app()
